32d0c0200628a8c5297dcf2de564383af23770b5
anand acchu <you@example.com>
2022-08-26 23:37:50 +0530

    welcome

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace LinkedListDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(45);
+            linkedList.Add(78);
+            linkedList.Add(48);
+            linkedList.Add(10);
+
+
+            LinkedList linkedList2 = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+
+            
+            linkedList.Display();
+        }
+    }
+}

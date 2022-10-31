7b956ddb95c2f05c0416bc690d9efda77a82b801
anandacchu <108655411+anandacchu@users.noreply.github.com>
2022-10-31 19:37:31 +0530

    uc4

--- LinkedList.cs
+++ LinkedList.cs
@@ -1,19 +1,36 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LinkedListDemo
+namespace LinkedListProblem
 {
-     public class LinkedList
+    public class LinkedList
     {
         Node head;
-
+        Node current;
         public void Add(int data)
         {
             Node node = new Node(data);
+            //no first node added to list
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                //head is asigned to new node
+                node.next = this.head;
+                this.head = node;
+            }
+            Console.WriteLine("{0} data is inserted ", node.data);
+        }
 
+        public void Append(int data)
+        {
+            Node node = new Node(data);
+            //no first node added to list
             if (this.head == null)
             {
                 this.head = node;
@@ -21,30 +38,53 @@ namespace LinkedListDemo
             else
             {
                 Node temp = head;
-
                 while (temp.next != null)
                 {
                     temp = temp.next;
+
                 }
+                //assigning node to temp->next
                 temp.next = node;
             }
-            Console.WriteLine("{0} inserted into Linked List ", node.data);
+            Console.WriteLine("{0} data is inserted ", node.data);
+        }
+
+        public void Insert(int data, int posVal)
+        {
+            Node node = new Node(data);
+            Node temp = this.head;
+
+            while (temp != null)
+            {
+                if (temp.data == 56)
+                {
+                    //next of 50 assigned to new node
+                    //new node is assigning to 50 
+                    node.next = temp.next;
+                    temp.next = node;
+                    Console.WriteLine("New data is inserted in between 56 and 70");
+                    break;
+
+                }
+                temp = temp.next;
+            }
         }
 
         public void Display()
         {
             Node temp = this.head;
+
             if (temp == null)
             {
-                Console.WriteLine("Linkedlist is empty");
+                Console.WriteLine("LinkedList is empty");
                 return;
             }
-
             while (temp != null)
             {
                 Console.WriteLine(temp.data + " ");
                 temp = temp.next;
             }
         }
+
     }
 }

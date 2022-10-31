e2e4867f13181a0c78cf759bb0a4a226d004d295
anandacchu <108655411+anandacchu@users.noreply.github.com>
2022-10-31 19:11:02 +0530

    Welcome To Linkedlist

--- Program.cs
+++ Program.cs
@@ -1,23 +1,11 @@
-﻿namespace LinkedListDemo
+
+namespace EmployeeWage
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            LinkedList linkedList = new LinkedList();
-            linkedList.Add(45);
-            linkedList.Add(78);
-            linkedList.Add(48);
-            linkedList.Add(10);
-
-
-            LinkedList linkedList2 = new LinkedList();
-            linkedList.Add(56);
-            linkedList.Add(30);
-            linkedList.Add(70);
-
-            
-            linkedList.Display();
+            Console.WriteLine("Welcome to Linked List Program");
         }
     }
 }
